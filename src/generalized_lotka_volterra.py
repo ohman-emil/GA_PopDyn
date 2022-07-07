d5ca19eaa17524dc436ac0dc1ce737c76fb97870
Emil0hman <kameragubben04@outlook.com>
2022-07-07 13:36:08 +0200

    Add generalized Lotka-Volterra model

--- src/generalized_lotka_volterra.py
+++ src/generalized_lotka_volterra.py
@@ -1,9 +1,8 @@
 import time
 time_start = time.perf_counter() # start timer
 
-from numpy import linspace
+from numpy import linspace, multiply, dot, add
 from scipy.integrate import solve_ivp
-import tikzplotlib
 import matplotlib.pyplot as plt
 import json
 import sys
@@ -19,18 +18,12 @@ plt.style.use('seaborn-whitegrid')
 plt.rcParams["figure.figsize"] = (16,8)
 
 # is there (still) a better data structre?
-data = json.load(open(f'./src/data/competitive_lotka_volterra/{sys.argv[1]}.json'))
+data = json.load(open(f'./src/data/generalized_lotka_volterra/{sys.argv[1]}.json'))
 
 # define model
+# TODO: refactor model. Should prob be able to do less linalg operations => faster?
 def func(t, y, data):
-    res = [] # one item for every species
-
-    for i in range(data['n']):
-        interactions = 0 # for the sum over k in the formula
-        for j, value in enumerate(data["interaction_matrix"][i]):
-            interactions += value * y[j]
-
-        res.append(data['r'][i] * y[i] * (1 - (interactions / data['k'][i]))) # formula (see https://en.wikipedia.org/wiki/Competitive_Lotka%E2%80%93Volterra_equations#N_species)
+    res = multiply(y, add(data['r'], dot(data['interaction_matrix'], y))) # formula from https://stefanoallesina.github.io/Sao_Paulo_School/intro.html
 
     return res
 
@@ -49,9 +42,9 @@ sol = solve_ivp(func, t_span, initial, method='LSODA', t_eval=t, args=(data,)) #
 for i, item in enumerate(sol.y):
     plt.plot(sol.t, item, label=roman.int_to_roman(i))
 
-plt.plot(sol.t, sum(sol.y), alpha=0.75, linestyle='--', label="SUM") # create a line with total biomass
+# plt.plot(sol.t, sum(sol.y), alpha=0.75, linestyle='--', label="SUM") # create a line with total biomass
 
-# plot results
+# plot settings
 plt.xlabel('time')
 plt.ylabel('biomass')
 plt.legend(loc="lower center", bbox_to_anchor=(0.5, 1), ncol=5, fontsize="xx-large")
@@ -63,8 +56,6 @@ print(f"\
     Module time: {round(time_modules-time_start, 5)} s\n\
     Execution time: {round(time_end-time_modules, 5)} s"
 )
-
-# tikzplotlib.save('test.tex')
 
 # show diagram
 plt.show()
